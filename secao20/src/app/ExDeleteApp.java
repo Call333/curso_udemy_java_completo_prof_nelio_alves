a78ec3390a69eeec6dd409ce4d461e07a541a6b6
Call333 <cam4@aluno.ifal.edu.br>
2024-07-02 09:31:36 -0300

    delete

--- secao20/src/app/ExDeleteApp.java
+++ secao20/src/app/ExDeleteApp.java
@@ -3,40 +3,34 @@ package secao20.src.app;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import secao20.src.db.DB;
+import secao20.src.db.DbIntegrityException;
 
-public class ExAtualizacaoApp {
+public class ExDeleteApp {
     public static void main(String[] args) {
-
         Connection conn = null;
         PreparedStatement st = null;
-    
-        try {
+
+        try{
             conn = DB.getConnection();
-            
             st = conn.prepareStatement(
-                "UPDATE seller "
-                + "SET BaseSalary = BaseSalary + ? "
-                + "WHERE (departmentId = ?)", 
-                Statement.RETURN_GENERATED_KEYS
+                "DELETE FROM department "
+                + "WHERE "
+                + "id = ?"
                 );
-    
-            st.setDouble(1, 200);
-            st.setInt(2, 1);
-    
+
+            st.setInt(1, 8);
+
             int rowsAffected = st.executeUpdate();
-    
             System.out.println("Rows Affected: " + rowsAffected);
         }
         catch(SQLException e) {
-            e.printStackTrace();
+            throw new DbIntegrityException(e.getMessage());
         }
-        finally{
+        finally {
             DB.closeStatement(st);
             DB.closeConnection();
         }
     }
-
 }
