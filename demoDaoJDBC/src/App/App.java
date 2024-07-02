626d222fe84438d5845903f431f8fd8a742b4528
Call333 <cam4@aluno.ifal.edu.br>
2024-07-02 11:34:56 -0300

    add a classe Seller

--- demoDaoJDBC/src/App/App.java
+++ demoDaoJDBC/src/App/App.java
@@ -1,10 +1,16 @@
 package demoDaoJDBC.src.App;
 
+import java.util.Date;
+
 import demoDaoJDBC.src.model.entities.Department;
+import demoDaoJDBC.src.model.entities.Seller;
 
 public class App {
     public static void main(String[] args) {
         Department obj = new Department(1, "Eletronics");
         System.out.println(obj);
+
+        Seller slr = new Seller(1, "Rose Marie", "roma@gmail.com", new Date(), 3000.00, obj);
+        System.out.println(slr);
     }
 }
